ea54b5d7613d079cdd7c5870c426452f7282b67c
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2022-07-28 16:11:03 +0200

    Add acceptance test failing
    Co-authored-by: Eduardo Alonso <eduardo.alonso@audiense.com>
    Co-authored-by: Manuel Pérez <Pandacore@users.noreply.github.com>

---
+++ src/UnusualSpendingsService.js
@@ -0,0 +1,5 @@
+export class UnusualSpendingsService {
+    constructor(unusualSpendingsDetector, alertSender) { }
+
+    run(userId) { }
+}
--- test/acceptance/unusual_spendings_service.test.js
+++ test/acceptance/unusual_spendings_service.test.js
@@ -1,5 +1,33 @@
+import { UnusualSpendingsDetector } from '../../src/UnusualSpendingsDetector';
+import { AlertSender } from '../../src/AlertSender';
+import { UnusualSpendingsService } from '../../src/UnusualSpendingsService';
+
 describe('UnusualSpendingsService', () => {
     test('alerting users with unusual spendings in some categories', () => {
-        expect(1).toStrictEqual(0);
+        const paymentsRepository = {find: async () => [
+            {amount: 100, category: 'food', month: '2020-01'},
+            {amount: 200, category: 'food', month: '2020-02'}
+        ]};
+        const calendar = {getCurrentMonth: () => '2020-02'};
+        const notifier = {notify: jest.fn()};
+        const usersRepository = {find: async () => [{email: 'user@mail.com'}]};
+        const unusualSpendingsDetector = new UnusualSpendingsDetector(paymentsRepository, calendar);
+        const alertSender = new AlertSender(usersRepository, notifier);
+        const unusualSpendingsService = new UnusualSpendingsService(unusualSpendingsDetector, alertSender);
+        const userId = '1234'
+
+        unusualSpendingsService.run(userId)
+
+        const expectedNotification = `Hello card user!
+
+        We have detected unusually high spending on your card in these categories:
+
+        * You spent $200 on food
+
+        Love,
+
+        The Credit Card Company`;
+
+        expect(notifier.notify).toHaveBeenCalledWith(expectedNotification)
     });
 });
