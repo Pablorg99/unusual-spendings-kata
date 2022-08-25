9a38467dd96e7f9347dddb03ce5418c74cd9c1a4
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2022-08-25 17:40:28 +0200

    Implement unusual spendings detector
    Co-authored-by: Fernando Vilas <vilasmaciel@users.noreply.github.com>

--- src/UnusualSpendingsDetector.js
+++ src/UnusualSpendingsDetector.js
@@ -5,19 +5,42 @@ export class UnusualSpendingsDetector {
   }
 
   run(userId) {
-    let month = this.calendar.getCurrentMonth();
-    let previousMonth = this.calendar.getPreviousMonth();
+    const month = this.calendar.getCurrentMonth();
+    const previousMonth = this.calendar.getPreviousMonth();
 
-    let paymentsMonth = this.paymentsRepository.find(userId, month);
-    let previousMonthPayments = this.paymentsRepository.find(
-      userId,
-      previousMonth
-    );
+    const paymentsMonth = this.paymentsRepository.find(userId, month);
+    const previousMonthPayments = this.paymentsRepository.find(userId, previousMonth);
 
-    if (previousMonthPayments.length === 0) {
+    if (previousMonthPayments.length === 0 || paymentsMonth.length === 0) {
       return [];
     }
 
-    return [{ amount: 200, category: 'food'}];
+    return computeUnusualSpendings(paymentsMonth, previousMonthPayments);
   }
+}
+
+function computeUnusualSpendings(paymentsMonth, previousMonthPayments) {
+  const categories = new Set(paymentsMonth.map(payment => payment.category));
+  const unusualSpendings = [];
+  for (const category of categories) {
+    const currentMonthCategorySpending = computeTotalSpendingByCategory(paymentsMonth, category);
+    const previousMonthCategorySpending = computeTotalSpendingByCategory(previousMonthPayments, category);
+    if (isThereUnusualSpending(previousMonthCategorySpending, currentMonthCategorySpending)) {
+      unusualSpendings.push({ category, amount: currentMonthCategorySpending });
+    }
+  }
+  return unusualSpendings;
+}
+
+function isThereUnusualSpending(previousSpending, currentSpending) {
+  return previousSpending > 0 && previousSpending * 1.5 <= currentSpending;
+}
+
+function computeTotalSpendingByCategory(payments, category) {
+  const paymentsForCategory = payments.filter(payment => payment.category === category);
+  return computeTotalSpending(paymentsForCategory);
+}
+
+function computeTotalSpending(payments) {
+  return payments.reduce((spending, payment) => spending + payment.amount, 0);
 }
--- test/unit/UnusualSpendingsDetector.test.js
+++ test/unit/UnusualSpendingsDetector.test.js
@@ -1,85 +1,151 @@
 import { UnusualSpendingsDetector } from '../../src/UnusualSpendingsDetector';
 
 describe('UnusualSpendingsDetector', () => {
-  xtest('should not return any payment when there are not any', () => {
-    const paymentsRepository = { find: jest.fn(() => []) };
-    const calendar = {
-      getCurrentMonth: () => '2020-02',
-      getPreviousMonth: () => '2020-01',
-    };
+  const currentMonth = '2020-02';
+  const previousMonth = '2020-01';
+  const calendar = {
+    getCurrentMonth: () => currentMonth,
+    getPreviousMonth: () => previousMonth,
+  };
+  const userId = '1234';
+
+  test('should not return any unusual spending when there are not any payments', () => {
+    const paymentsRepository = getPaymentsRepositoryReturning([], []);
     const unusualSpendingsDetector = new UnusualSpendingsDetector(
       paymentsRepository,
       calendar
     );
-    const userId = '1234';
 
     const unusualSpendings = unusualSpendingsDetector.run(userId);
 
-    expect(paymentsRepository.find).toHaveBeenCalledWith(userId, '2020-02');
-    expect(paymentsRepository.find).toHaveBeenCalledWith(userId, '2020-01');
     expect(unusualSpendings).toEqual([]);
   });
 
-  test('should not return any payments when there are not previous payments', () => {
-    const paymentsMonth = [
-      paymentWith('food', 200, '2020-02'),
-      paymentWith('transport', 100, '2020-02'),
-    ];
-    const paymentsPrevious = [];
-    const paymentsRepository = {
-      find: jest.fn().mockImplementation((userId, month) => {
-        if (userId === '1234' && month === '2020-02') {
-          return paymentsMonth;
-        } else if (userId === '1234' && month === '2020-01') {
-          return paymentsPrevious;
-        }
-      }),
-    };
-    const calendar = {
-      getCurrentMonth: () => '2020-02',
-      getPreviousMonth: () => '2020-01',
-    };
+  test('should return an unusual spending when there has been an increase between month payments', () => {
+    const paymentsMonth = [paymentWith('food', 200, currentMonth)];
+    const paymentsPrevious = [paymentWith('food', 100, previousMonth)];
+    const expectedUnusualSpendings = [unusualSpendingWith('food', 200)];
+    const paymentsRepository = getPaymentsRepositoryReturning(
+      paymentsMonth,
+      paymentsPrevious
+    );
     const unusualSpendingsDetector = new UnusualSpendingsDetector(
       paymentsRepository,
       calendar
     );
-    const userId = '1234';
 
     const unusualSpendings = unusualSpendingsDetector.run(userId);
 
-    expect(paymentsRepository.find).toHaveBeenCalledWith(userId, '2020-02');
-    expect(paymentsRepository.find).toHaveBeenCalledWith(userId, '2020-01');
-    const expectedUnusualSpendings = []
     expect(unusualSpendings).toEqual(expectedUnusualSpendings);
   });
 
-  test('should not return any payments when there are not previous payments', () => {
-    const paymentsMonth = [ paymentWith('food', 200, '2020-02') ];
-    const paymentsPrevious = [ paymentWith('food', 100, '2020-01') ];
-    const paymentsRepository = {
-      find: jest.fn().mockImplementation((userId, month) => {
-        if (userId === '1234' && month === '2020-02') {
-          return paymentsMonth;
-        } else if (userId === '1234' && month === '2020-01') {
-          return paymentsPrevious;
-        }
-      }),
-    };
-    const calendar = {
-      getCurrentMonth: () => '2020-02',
-      getPreviousMonth: () => '2020-01',
-    };
+  test('should not return any unusual spending when there are not payments in current month', () => {
+    const paymentsMonth = [];
+    const paymentsPrevious = [
+      paymentWith('food', 200, currentMonth),
+    ];
+    const expectedUnusualSpendings = [];
+    const paymentsRepository = getPaymentsRepositoryReturning(
+      paymentsMonth,
+      paymentsPrevious
+    );
     const unusualSpendingsDetector = new UnusualSpendingsDetector(
       paymentsRepository,
       calendar
     );
-    const userId = '1234';
 
     const unusualSpendings = unusualSpendingsDetector.run(userId);
 
-    expect(paymentsRepository.find).toHaveBeenCalledWith(userId, '2020-02');
-    expect(paymentsRepository.find).toHaveBeenCalledWith(userId, '2020-01');
-    const expectedUnusualSpendings = [unusualSpendingWith('food', 200)]
+    expect(unusualSpendings).toEqual(expectedUnusualSpendings);
+  });
+
+  test('should not return any unusual spending when there is not any previous payment for a category', () => {
+    const paymentsMonth = [paymentWith('transport', 200, currentMonth)];
+    const paymentsPrevious = [paymentWith('food', 200, currentMonth)];
+    const expectedUnusualSpendings = [];
+    const paymentsRepository = getPaymentsRepositoryReturning(
+      paymentsMonth,
+      paymentsPrevious
+    );
+    const unusualSpendingsDetector = new UnusualSpendingsDetector(
+      paymentsRepository,
+      calendar
+    );
+
+    const unusualSpendings = unusualSpendingsDetector.run(userId);
+
+    expect(unusualSpendings).toEqual(expectedUnusualSpendings);
+  });
+
+  test.each([[100, 100], [149.99, 100]])('should not return any unusual spending when there is not an increase greater or equal than 50% between month payments', (currentMonthPaymentAmount, previousMonthPaymentAmount) => {
+    const paymentsMonth = [paymentWith('food', currentMonthPaymentAmount, currentMonth)];
+    const paymentsPrevious = [paymentWith('food', previousMonthPaymentAmount, previousMonth)];
+    const expectedUnusualSpendings = [];
+    const paymentsRepository = getPaymentsRepositoryReturning(
+      paymentsMonth,
+      paymentsPrevious
+    );
+    const unusualSpendingsDetector = new UnusualSpendingsDetector(
+      paymentsRepository,
+      calendar
+    );
+
+    const unusualSpendings = unusualSpendingsDetector.run(userId);
+
+    expect(unusualSpendings).toEqual(expectedUnusualSpendings);
+  });
+
+  test('should return an unusual spending when there has been an increase between month payments with multiple payments in current month', () => {
+    const paymentsMonth = [paymentWith('food', 100, currentMonth), paymentWith('food', 100, currentMonth)];
+    const paymentsPrevious = [paymentWith('food', 100, previousMonth)];
+    const expectedUnusualSpendings = [unusualSpendingWith('food', 200)];
+    const paymentsRepository = getPaymentsRepositoryReturning(
+      paymentsMonth,
+      paymentsPrevious
+    );
+    const unusualSpendingsDetector = new UnusualSpendingsDetector(
+      paymentsRepository,
+      calendar
+    );
+
+    const unusualSpendings = unusualSpendingsDetector.run(userId);
+
+    expect(unusualSpendings).toEqual(expectedUnusualSpendings);
+  });
+
+  test('should not return any unusual spending when there aren\'t increases between months with multiple payments in previous month', () => {
+    const paymentsMonth = [paymentWith('food', 150, previousMonth)];
+    const paymentsPrevious = [paymentWith('food', 100, currentMonth), paymentWith('food', 100, currentMonth)];
+    const expectedUnusualSpendings = [];
+    const paymentsRepository = getPaymentsRepositoryReturning(
+      paymentsMonth,
+      paymentsPrevious
+    );
+    const unusualSpendingsDetector = new UnusualSpendingsDetector(
+      paymentsRepository,
+      calendar
+    );
+
+    const unusualSpendings = unusualSpendingsDetector.run(userId);
+
+    expect(unusualSpendings).toEqual(expectedUnusualSpendings);
+  });
+
+  test('should compute unusual spendings for multiple categories', () => {
+    const paymentsMonth = [paymentWith('food', 100, previousMonth), paymentWith('transport', 200, previousMonth)];
+    const paymentsPrevious = [paymentWith('food', 100, currentMonth), paymentWith('transport', 100, currentMonth)];
+    const expectedUnusualSpendings = [unusualSpendingWith('transport', 200)];
+    const paymentsRepository = getPaymentsRepositoryReturning(
+      paymentsMonth,
+      paymentsPrevious
+    );
+    const unusualSpendingsDetector = new UnusualSpendingsDetector(
+      paymentsRepository,
+      calendar
+    );
+
+    const unusualSpendings = unusualSpendingsDetector.run(userId);
+
     expect(unusualSpendings).toEqual(expectedUnusualSpendings);
   });
 
@@ -87,8 +153,22 @@ describe('UnusualSpendingsDetector', () => {
     return { category, amount, month };
   }
 
+  function unusualSpendingWith(category, amount) {
+    return { category, amount };
+  }
 
-function unusualSpendingWith(category , amount) {
-  return { category, amount };
-}
+  function getPaymentsRepositoryReturning(
+    currentMonthPayments,
+    previousMonthPayments
+  ) {
+    return {
+      find: jest.fn().mockImplementation((userId, month) => {
+        if (month === currentMonth) {
+          return currentMonthPayments;
+        } else if (month === previousMonth) {
+          return previousMonthPayments;
+        }
+      }),
+    };
+  }
 });
